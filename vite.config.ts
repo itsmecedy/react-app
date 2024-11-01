8833fcf6e46fdacb0bb053192848a3f222b72d9b
@itsmecedy <91381330+Sidibuy@users.noreply.github.com>
2024-11-02 00:27:19 +0800

    test component

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  esbuild: {
+    loader: 'tsx',
+    include: /src\/.*\.tsx?$/, // Adjust the regex if your files are in different folders
+  },
+});
